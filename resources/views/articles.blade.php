394969b2d601b1d3f8a876069bd379e9756d3300
James Lin <james.lin@yellow.co.nz>
2018-01-12 13:44:05 +1300

    redirect home to article list, if no articles display create article link

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($articles as $article)
-    <div class="row">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-12">
-                <p><h2>{{ link_to_route('article', $article->title, $parameters = ['slug'=>$article->slug]) }}</h2></p>
+    @if (count($articles) == 0 )
+        <p>There are no articles, please {{ link_to_route('createForm', 'create one') }}</p>
+    @else
+        @foreach ($articles as $article)
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12">
+                    <p><h2>{{ link_to_route('article', $article->title, $parameters = ['slug'=>$article->slug]) }}</h2></p>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <p>{{ str_limit($article->content, 50) }}...</p>
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>{{ str_limit($article->content, 50) }}...</p>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <p>by {{ $article->user->name }}, at {{ $article->created_at }}</p>
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>by {{ $article->user->name }}, at {{ $article->created_at }}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    @endforeach
+        @endforeach
+    @endif
 @endsection
